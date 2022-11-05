5cd8ccb5d816387e49dc89fe9b9489ddccb027b1
xX-DARKLEVEL-Xx <darklevel_contact@protonmail.com>
2022-11-05 15:51:28 +0100

    Update docs

---
+++ assets/js/docs.js
@@ -0,0 +1,23 @@
+
+
+let newA = document.createElement('a'),
+    newTitle = document.createElement('p'),
+    newDescription = document.createElement('p'),
+    newDiv = document.createElement('div')
+
+class CreateDocument {
+    constructor(title, description){
+        const docList = document.getElementById("docList")
+
+        this.title = title
+        this.description = description
+        this.container = docList
+
+    }
+    create(){
+        newTitle.textContent = this.title
+        newDescription.textContent = this.title
+        this.container.append(newA.append(newTitle,newDiv.append(newDescription)))
+    }
+}
+CreateDocument.create("hello","this is the description")
